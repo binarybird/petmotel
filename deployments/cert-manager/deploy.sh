4c8332e44e9b729071236a4f667380d050419cf2
binarybird <jamesrichardson2@gmail.com>
2021-04-29 18:53:18 -0500

    Adjustments

--- deployments/cert-manager/deploy.sh
+++ deployments/cert-manager/deploy.sh
@@ -5,5 +5,6 @@
 #  --version v1.3.1 \
 kubectl create namespace cert-manager
 kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.3.1/cert-manager.yaml
-sleep 15
+sleep 30
 kubectl get pods --namespace cert-manager
+kubectl apply -f lets-encrypt-staging-ca-cluster-issuer.yml
--- deployments/rabbitmq/deploy.sh
+++ deployments/rabbitmq/deploy.sh
@@ -1,4 +1,5 @@
 kubectl apply -f "https://github.com/rabbitmq/cluster-operator/releases/latest/download/cluster-operator.yml"
+sleep 30
 kubectl apply -f cluster.yml
 sleep 30
 username="$(kubectl get secret hello-world-default-user -o jsonpath='{.data.username}' | base64 --decode)"
